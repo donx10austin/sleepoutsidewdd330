8f8211850770b61d8378b323edbc34ebedb47ccf
donx10austin <austinanumudu85@gmail.com>
2026-07-17 14:58:16 +0100

    chore: format all project files to resolve linting errors

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // src/js/main.js
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { qs, addToWishlist } from './utils.mjs';
+import { qs, addToWishlist } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const listElement = qs(".product-list");
@@ -9,19 +9,23 @@ const myList = new ProductList("tents", dataSource, listElement);
 
 async function init() {
   await myList.init();
-  
+
   // Event Delegation: Listen for clicks on the list, filter for the button
   listElement.addEventListener("click", (e) => {
     if (e.target.classList.contains("wishlist-btn")) {
-       const btn = e.target;
-       const product = {
-         id: btn.dataset.id,
-         name: btn.dataset.name,
-         price: btn.dataset.price,
-         image: btn.dataset.img
-       };
-       const wasAdded = addToWishlist(product);
-       alert(wasAdded ? `🎉 ${product.name} added!` : `👀 ${product.name} is already there!`);
+      const btn = e.target;
+      const product = {
+        id: btn.dataset.id,
+        name: btn.dataset.name,
+        price: btn.dataset.price,
+        image: btn.dataset.img,
+      };
+      const wasAdded = addToWishlist(product);
+      alert(
+        wasAdded
+          ? `🎉 ${product.name} added!`
+          : `👀 ${product.name} is already there!`,
+      );
     }
   });
 }
